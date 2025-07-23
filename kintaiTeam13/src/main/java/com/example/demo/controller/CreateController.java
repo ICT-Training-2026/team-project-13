258eb6f810e1c8d12dcde6260c737ad6a6004611
github chisako-a <chisako-fujimoto@kccs.co.jp>
2025-07-23 11:33:06 +0900

    新規社員登録機能にvalidationを追加

--- kintaiTeam13/src/main/java/com/example/demo/controller/CreateController.java
+++ kintaiTeam13/src/main/java/com/example/demo/controller/CreateController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.form.CreateForm;
 
 @Controller
-public class Create {
+public class CreateController {
   
 	/*新規登録画面表示リクエスト
 	 
@@ -28,16 +30,26 @@ public class Create {
 	 
 	 /*新規登録リクエスト（登録画面から確認へ）*/
 	  @PostMapping("/create-user")
-	  public String createUser(@ModelAttribute CreateForm form) {
+	  public String createUser(@Validated @ModelAttribute CreateForm form,
+			  BindingResult result) {
 		 
+		  if (result.hasErrors()) {
+			  return "create_user";
+		  }
 		  return "confirm_create_user";
 	  }
 	 
 	 /*新規登録リクエスト（確認画面から完了）*/
 	  @PostMapping("/confirm-create-user")
-	  public String confirmCreateUser(@ModelAttribute CreateForm form, Model model) {
+	  public String confirmCreateUser(@Validated /*@ModelAttribute*/ CreateForm form, 
+			  BindingResult result, Model model) {
+		  
+		  if (result.hasErrors()) {
+			  return "create_user";
+		  }
+		  
 		  model.addAttribute("msg", "新規社員登録が完了しました。");
 		  return "complete_create_user";
 	  }
-	 
 }
+
--- kintaiTeam13/src/main/java/com/example/demo/form/CreateForm.java
+++ kintaiTeam13/src/main/java/com/example/demo/form/CreateForm.java
@@ -1,13 +1,31 @@
 package com.example.demo.form;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class CreateForm {
 
-	private Integer employeeId;
-	private String name;
-	private Integer departmentId;
-	private String pass;
-	
+    @NotNull(message = "入力してください。")
+    @Min(value = 100000, message = "社員IDは6桁の数値で入力してください。") // 例: 100000 から 999999 の範囲を指定
+    @Max(value = 999999)
+    private Integer employeeId;
+
+    @NotNull(message = "入力してください。")
+    @Size(max = 50, message = "名前は50文字以内で入力してください。")
+    private String name;
+
+    @NotNull(message = "入力してください。")
+    @Min(value = 1000, message = "部署IDは4桁の数値で入力してください。") // 例: 1000 から 9999 の範囲を指定
+    @Max(value = 9999)
+    private Integer departmentId;
+
+    @NotNull(message = "入力してください。")
+    @Size(max = 16, message = "パスワードは16文字以下で入力してください。")
+    private String pass;
+
 }
