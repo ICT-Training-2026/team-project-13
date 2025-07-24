c5b6c1f43723805de03cfa7e3e8de10b105749c0
github chisako-a <chisako-fujimoto@kccs.co.jp>
2025-07-24 14:31:16 +0900

    削除機能追加

--- kintaiTeam13/src/main/java/com/example/demo/repository/DeleteRepositoryImpl.java
+++ kintaiTeam13/src/main/java/com/example/demo/repository/DeleteRepositoryImpl.java
@@ -11,17 +11,63 @@ public class DeleteRepositoryImpl implements DeleteRepository {
 	
 	public final JdbcTemplate jdbcTemplate;
 
+//	@Override
+//	public void deleteStamp(int registId) {
+//		
+//		String sql =
+//				" DELETE              " + 
+//				" FROM                " + 
+//				"  attendance_info    " + 
+//				" WHERE               " + 
+//				"   REGIST_ID = ?     "; 
+//		
+//		jdbcTemplate.update(sql, registId);
+//	}
+//
+//}
 	@Override
 	public void deleteStamp(int registId) {
-		
-		String sql =
-				" DELETE              " + 
-				" FROM                " + 
-				"  attendance_info    " + 
-				" WHERE               " + 
-				"   REGIST_ID = ?     "; 
-		
-		jdbcTemplate.update(sql, registId);
-	}
+	    // SQL to check if the status_id is 2, 3, or 4
+	    String checkStatusSql = 
+	            "SELECT status_id FROM attendance_info " +
+	            "WHERE REGIST_ID = ?";
 
+	    // Get the status_id for the given registId
+	    Integer statusId = jdbcTemplate.queryForObject(checkStatusSql, Integer.class, registId);
+
+	    if (statusId != null) {
+	        if (statusId == 2) {
+	            // If status_id is 2, decrement furi_holiday in dayoff table
+	            String updateDayoffSql = 
+	                    "UPDATE dayoff " +
+	                    "SET furi_holiday = furi_holiday - 1 " +
+	                    "WHERE employee_id = (SELECT employee_id FROM attendance_info WHERE REGIST_ID = ?)";
+
+	            jdbcTemplate.update(updateDayoffSql, registId);
+	        } else if (statusId == 3) {
+	            // If status_id is 3, increment furi_holiday in dayoff table
+	            String updateDayoffSql = 
+	                    "UPDATE dayoff " +
+	                    "SET furi_holiday = furi_holiday + 1 " +
+	                    "WHERE employee_id = (SELECT employee_id FROM attendance_info WHERE REGIST_ID = ?)";
+
+	            jdbcTemplate.update(updateDayoffSql, registId);
+	        } else if (statusId == 4) {
+	            // If status_id is 4, increment yu_holiday in dayoff table
+	            String updateDayoffSql = 
+	                    "UPDATE dayoff " +
+	                    "SET yu_holiday = yu_holiday + 1 " +
+	                    "WHERE employee_id = (SELECT employee_id FROM attendance_info WHERE REGIST_ID = ?)";
+
+	            jdbcTemplate.update(updateDayoffSql, registId);
+	        }
+	    }
+
+	    // SQL to delete the record
+	    String deleteSql = 
+	            "DELETE FROM attendance_info " +
+	            "WHERE REGIST_ID = ?";
+
+	    jdbcTemplate.update(deleteSql, registId);
+	}
 }
